3deaa462aa15a8b4d2fabb8e02a6fa51dd876f46
Alice Petrov <17ap87@queensu.ca>
2022-12-23 21:17:45 -0500

    Templates

--- semigroups4planning/gap.py
+++ semigroups4planning/gap.py
@@ -20,14 +20,24 @@ class GAP():
         self._environment = Environment(loader=FileSystemLoader(os.path.join(HERE, "templates/")))
 
     
-    def create_transformation_semigroup(self, generators, var: str = "S"):
+    def create_transformation_semigroup(self, generators, transformation_semigroup: str = "S"):
         """
         """
         transformations = [np.array2string(v, separator=', ', max_line_width=sys.maxsize) for v in generators]
         
-        template = self._environment.get_template("transformation_semigroup.txt")
-        content = template.render(var = var, generators = transformations)
+        template = self._environment.get_template("functions/transformation_semigroup.txt")
+        content = template.render(transformation_semigroup = transformation_semigroup, generators = transformations)
+
+        # TODO call GAP with content
 
         # with open("test.txt", mode="w", encoding="utf-8") as message: # TODO remove after testing
         #     message.write(content)
+
+    def get_basic_attributes(self, generators, transformation_semigroup: str = "S"):
+        """
+        """
+        transformations = [np.array2string(v, separator=', ', max_line_width=sys.maxsize) for v in generators]
+        
+        template = self._environment.get_template("basic_attributes.txt")
+        content = template.render(transformation_semigroup = transformation_semigroup, generators = transformations)
         
