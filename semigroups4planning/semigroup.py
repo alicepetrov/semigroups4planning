ff3c0a587e676c94439e4a40c706f6fa6ee6c734
Alice Petrov <petrovalice@gmail.com>
2023-01-05 12:19:19 -0500

    Sync files

---
+++ semigroups4planning/semigroup.py
@@ -0,0 +1,22 @@
+"""
+"""
+
+class TransformationSemigroup():
+    """
+    """
+    def __init__(
+        self,
+        actions
+    ):
+        self._actions = actions
+        self._generators = [v for v in actions.values()]
+
+
+    def get_transformation_kernel(): # TODO
+        pass
+
+    def get_transformation_image(): # TODO
+        pass
+
+    def get_transformation_rank(): # TODO
+        pass
